5de863af2fd8091498f7fbc02880e93aabf7603d
igoryouiti <igoryouiti@hotmail.com>
2022-08-01 15:07:14 -0300

    model para temas

---
+++ src/main/java/com/blogpessoal/blogpessoal/model/Tema.java
@@ -0,0 +1,37 @@
+package com.blogpessoal.blogpessoal.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String descricao;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
