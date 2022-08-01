5f89d2b7d05fb7568d6c5bda3d7d6553658e77aa
igoryouiti <igoryouiti@hotmail.com>
2022-08-01 11:45:23 -0300

    rota para deletar postagem por id

--- src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/postagens")
@@ -48,6 +50,17 @@ public class PostagemController {
                 .map(resposta -> ResponseEntity.status(HttpStatus.OK)
                         .body(postagemRepository.save(postagem)))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        Optional<Postagem> postagem = postagemRepository.findById(id);
+
+        if(postagem.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        postagemRepository.deleteById(id);
     }
 
 }
