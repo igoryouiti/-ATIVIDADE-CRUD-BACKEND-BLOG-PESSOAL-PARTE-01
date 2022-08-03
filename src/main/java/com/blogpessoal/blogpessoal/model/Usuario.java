a32cd85eed778ad68aca45858b7f9c90d64878bf
igoryouiti <igoryouiti@hotmail.com>
2022-08-03 07:06:33 -0300

    atributos e anotaÃcoes para entidade  usuario

--- src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
+++ src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
@@ -1,4 +1,77 @@
 package com.blogpessoal.blogpessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String nome;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String usuario;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String senha;
+
+    @Size(min = 3, max = 500)
+    private String foto;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List<Postagem> postagens;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
