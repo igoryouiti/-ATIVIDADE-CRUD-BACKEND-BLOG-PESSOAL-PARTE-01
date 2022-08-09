423f3d6eac914166618ef4f267eee51465686a32
igoryouiti <igoryouiti@hotmail.com>
2022-08-09 11:02:37 -0300

    adição de método para busca de usuario

--- src/main/java/com/blogpessoal/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal/repository/UsuarioRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public List<Usuario> findAllByUsuarioContainingIgnoreCase(@Param("usuario") String usuario);
+
+    public Optional<Usuario> findByUsuario(String userName);
 }
